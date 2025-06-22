5c57cb121d25586c25912bdd0e2ab828af8847e8
Abhishek Kumar <146465745+42Abhishek@users.noreply.github.com>
2025-06-22 12:32:59 +0530

    Update axiosClient.js

--- Frontend/src/axiosClient.js
+++ Frontend/src/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL : "http://localhost:5000",
+    baseURL : "https://securenoteapp-backend.onrender.com",
     withCredentials: true,
     headers :{
         'Content-Type' : "application/json"
